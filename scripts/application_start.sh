0d79a95cfafc14386a78a817763bb8e1fcde3abd
Brian A <b.arayathel@gmail.com>
2023-03-17 13:38:54 -0400

    converted to express

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+echo 'run application_start.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 
-pm2 start npm --name "index.js" -- run dev
+echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+pm2 restart nodejs-express-app >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
