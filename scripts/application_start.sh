96c6c1ffe822f9448f91786ea9f6a6c769a44df2
Brian A <b.arayathel@gmail.com>
2023-03-17 12:14:11 -0400

    updated application start to use pm2

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-npm index.js
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+pm2 start npm --name "index.js" -- run dev
